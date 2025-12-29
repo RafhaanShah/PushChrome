4ec6db07bc705753743084e5e5fa34b5ec747a00
Raf <RafhaanShah@users.noreply.github.com>
2025-12-30 02:07:26 +0500

    Step 10.19: Refactor / Unify Styling

--- src/pages/messages.js
+++ src/pages/messages.js
@@ -200,12 +200,7 @@ function createMessageElement(msg, isUnread) {
         <div class="message-time" title="${new Date(msg.date * 1000).toLocaleString()}">${timeText}</div>
         ${isUnread ? '<div class="unread-indicator"></div>' : ''}
       </div>
-      <button class="message-delete" title="Delete message">
-        <svg width="14" height="14" viewBox="0 0 16 16" fill="currentColor">
-          <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-          <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-        </svg>
-      </button>
+      <button class="message-delete" title="Delete message">🗑️</button>
     </div>
     <div class="message-title-row">
       <span class="message-title">${escapeHtml(titleText)}</span>
